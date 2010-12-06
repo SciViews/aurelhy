5f9033a0c59420b09551a7ffbacaea8e06abd19f
phgrosjean <phgrosjean@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-12-06 21:56:03 +0000

    Final changes for FAO

--- R/utilities.R
+++ R/utilities.R
@@ -46,3 +46,82 @@
 # New resample method
 "resample" <- function (x, ...)
 	UseMethod("resample")
+
+# New add.points method
+"add.points" <- function (x, ...)
+	UseMethod("add.points")
+	
+# Augment a geomask with points near geopoints coordinates
+"add.points.geomask" <- function (x, geopoints, ...)
+{
+	# Check arguments
+	if (!inherits(x, "geomask"))
+		stop("'x' must be a 'geomask' object")
+	if (!inherits(geopoints, "geopoints"))
+		stop("'geopoints' must be a 'geopoints' object")
+	# Look for the points that are closest to the geomask grid in the 'geopoints' object
+	# Quick calculation by transforming coordinates into their closest index
+	# in the grid
+	Coords <- coords(x)
+	X = (geopoints$x - Coords["x"]) / Coords["size"]
+	Y = (geopoints$y - Coords["y"]) / Coords["size"]
+	# Round values for X and Y to match a grid point
+	Xind = round(X) + 1
+	Yind = round(Y) + 1
+	# Eliminate points that are outside the grid
+	In <- rep(TRUE, length(X))
+	In[Xind < 1] <- FALSE
+	In[Xind > nrow(x)] <- FALSE
+	In[Yind < 1] <- FALSE
+	In[Yind > ncol(x)] <- FALSE
+
+	# Check which point is inside the grid, but not in the mask (avoid negative
+	# indices... points eliminated anyway)
+	toAdd <- !diag(x[abs(Xind), abs(Yind)]) & In
+	# Add these points to the mask
+	added <- data.frame(x = Xind[toAdd], y = Yind[toAdd])
+	x[added$x, added$y] <- TRUE
+	# ... and record the list of added points as "added" attribute
+	added2 <- attr(x, "added")
+	if (is.null(added2)) {
+		attr(x, "added") <- added 
+	} else {
+		# Merge added points
+		attr(x, "added") <- rbind(added2, added)
+	}
+	return(x)
+}
+
+# Distance to the sea from DEM
+"dist2sea" <- function (geotm)
+{
+	# Edges: find sea pixels that have at least one terrestrian neighbour
+	"externaledge" <- function (geotm, o) {
+		edgematrix <- geotm
+		edgematrix[edgematrix >= 0] <- 0
+		for (i in 1:nrow(o)) {
+		    if (o[i, 1] == 1) x1 <- o[i, 1] else x1 <- o[i, 1] - 1
+		    if (o[i, 1] == nrow(geotm)) x2 <- o[i, 1] else x2 <- o[i, 1] + 1
+		    if (o[i, 2] == 1) y1 <- o[i, 2] else y1 <- o[i, 2] - 1
+		    if (o[i, 2] == ncol(geotm)) y2 <- o[i, 2] else y2 <- o[i, 2] + 1
+		    if (min(is.na(geotm[x1:x2, y1:y2])) == 0)
+				edgematrix[o[i, 1], o[i, 2]] <- 1
+		}
+		return(edgematrix)
+	}
+
+	dist2seaout <- geotm
+    o <- which(is.na(dist2seaout), arr.ind = TRUE)
+    edgematrix <- externaledge(dist2seaout, o)
+    xyland <- which(edgematrix == 0,  arr.ind = TRUE)
+    xyedge <- which(edgematrix == 1,  arr.ind = TRUE)
+    cellsize <- coords(geotm)["size"]
+    lat0 <- coords(geotm)["y1"]
+    for (i in 1:nrow(xyland)) {
+        dist2seaout[xyland[i, 1], xyland[i, 2]] <- min(sqrt(((xyland[i, 1] -
+			xyedge[, 1]) * cellsize * 111.2 * cos((xyland[i,2] *
+			cellsize + lat0) * pi / 180))^2 + ((xyland[i,2] - xyedge[,2]) *
+			cellsize * 111.2)^2))
+    }
+    return(dist2seaout)
+}
